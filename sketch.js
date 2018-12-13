02ecc6962c5fb81d73bd964faffbdfee5c90f1ec
Marvin Valenzuela <bend_@outlook.com>
2018-12-12 21:52:02 -0600

    tiles are now drawn according to board

--- sketch.js
+++ sketch.js
@@ -1,49 +1,37 @@
 
 let boardWidth;
 let board;
+let tiles;
 let size;
 
 function setup(){
-    boardWidth = 10;
+    boardWidth = 8;
     size = 50;
     createCanvas(boardWidth * size, boardWidth * size);
     board = [];
+    tiles = [];
     reset();
 }
 
 function reset(){
     board = [];
+    tiles = [];
+    let row = [];
+    let random = 0;
     for (let y = 0; y < boardWidth; y++){
-        let row = [];
+        row = [];
         for (let x = 0; x < boardWidth; x++){
-            row.push(1 + floor(Math.random() * 5));
+            random = 1 + floor(Math.random() * 5);
+            row.push();
+            tiles.push(new Tile(x * size, y * size, random));
         }
         board.push(row);
     }
 }
 
 function drawBoard(){
-    for (let y = 0; y < boardWidth; y++){
-        for (let x = 0; x < boardWidth; x++){
-            switch (board[y][x]){
-                case 1:
-                fill(225,0,0);
-                break;
-                case 2:
-                fill(0,255,0);
-                break;
-                case 3:
-                fill(0,0,255);
-                break;
-                case 4:
-                fill(225,0,255);
-                break;
-                case 5:
-                fill(0,225,255);
-                break;
-            }
-            rect(x * size, y * size, size - 2, size - 2);
-        }
+    for (let tile of tiles){
+        tile.show();
     }
 }
 
